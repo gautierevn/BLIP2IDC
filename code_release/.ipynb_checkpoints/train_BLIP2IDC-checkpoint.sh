12dfdee57c769bd859a03ab70bb3e773bfe58a1e
Gautier Evennou <gautier.evennou@gmail.com>
2024-03-07 13:21:42 +0100

    rename

--- code_release/.ipynb_checkpoints/train_BLIP2IDC-checkpoint.sh
+++ code_release/.ipynb_checkpoints/train_BLIP2IDC-checkpoint.sh
@@ -2,13 +2,12 @@
 
 source /your_venv/bin/activate 
 
-EXEC_FILE=/code_release/train_BLIP2.py
+EXEC_FILE=/code_release/train_BLIP2IDC.py
 
 echo =============== RUN ${OAR_JOB_ID} ===============
 echo Run $EXEC_FILE at $(date +"%T, %d-%m-%Y")
 python $EXEC_FILE --model_type "opt" --lora_rank 32 \
-    --TRAIN "False" --TEST "True" --save_dir "dir" --little_name "name" --select_rate 8 \
-    --ckpt "the/model/you/want/to/eval.bin.opt"
+    --TRAIN "True" --TEST "True" --save_dir "dir" --little_name "name" --select_rate 8 \
     --lr 4e-5 --module_to_ft "['LLM',QFormer,'ViT']" --vit_embeddings "False" --dataset "emu" \
     --synth_pretraining "False" --batch_size 32
 done
